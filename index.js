82acecd8a8a2e42daebd9d0d264a63263abef1cb
Nate Clark <n8@n8io.com>
2019-07-03 08:35:04 -0400

    ✨ Simplify command

--- index.js
+++ index.js
@@ -19,16 +19,19 @@ const sendPath = async tmpPath => {
     path = `/${tmpPath}`;
   }
 
-  try {
-    await got.post({
-      url: uri,
-      body: { debug: DRY_RUN ? true : undefined, path },
-      json: true
-    });
-  } catch (e) {
-    console.error('Error while sending request');
-    console.error(e);
-  }
+  return library;
+};
+
+const tranformPath = path => {
+  const reg = /([/]mnt[/]x[/]decrypted[/](tv|movies)[/](adult|release|prerelease))[/].*[/]/g;
+
+  const validPath = ((path || '').match(reg) || [])[0];
+
+  return validPath;
+};
+
+const makeCommand = (path, library) => {
+  return `LD_LIBRARY_PATH="/usr/lib/plexmediaserver" PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="/home/plexmediaserver/Library/Application Support" "/usr/lib/plexmediaserver/Plex Media Scanner" --scan --no-thumbs --section ${library} --directory "${path}"`;
 };
 
 const log = msg => {
@@ -47,11 +50,43 @@ const log = msg => {
   console.log(`${dt}: ${msg}`);
 };
 
-// One-liner for current directory, ignores .dotfiles
-chokidar.watch(WATCH_DIR).on('unlink', path => {
-  log(path);
-  sendPath(path);
-});
+const handleRequest = (req, res) => {
+  const { debug, path: tmpPath } = req.body;
+
+  const path = tranformPath(tmpPath);
+
+  if (!path) {
+    return res.status(400).json({ message: 'No path provided in the payload' });
+  }
+
+  log(`input ${tmpPath}`);
+
+  const library = parseLibrary(path);
+
+  if (!library) {
+    return res
+      .status(400)
+      .json({ message: 'Unable to determine library from path', path });
+  }
+
+  const command = makeCommand(path, library);
+
+  log(`path ${path}`);
+  log(`command ${command}`);
+
+  if (!debug) {
+    exec(command);
+  } else {
+    log('Debug enabled. Dry run only.');
+  }
+
+  return res.json({ message: 'OK', path, command });
+};
+
+app.use(bodyParser.json());
+
+app.post('/', handleRequest);
+app.get('/', (_, res) => res.json({ message: 'OK' }));
 
 log(`Paths will be posted to ${uri}`);
 log(`Watcher started for ${WATCH_DIR} ...`);
